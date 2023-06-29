b67c84b1ab0037709406c9478972ab1ac056ce12
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-06-29 22:12:05 +0300

    chore: titles

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -17,12 +17,12 @@ export default async function IndexPage({ params: { lang } }) {
   const dictionary = await getDictionary(lang)
 
   // map the array and return the card component
-  const cardList = dictionary.cards.map((card: { title: string; description: string; example: string; first: string; }) => {
+  const cardList = dictionary.cards.map((card: { title: string; description: string; example: string; first: string; slug:string }) => {
     // destructure the card object
-    const { title, description, example, first } = card
+    const { title, description, example, first, slug } = card
     // return the card component
     return (
-      <Card key={title}>
+      <Card key={slug}>
         <CardHeader>
           <CardTitle className="text-center">{title}</CardTitle>
           {/* <CardDescription>{description}</CardDescription> */}
@@ -40,23 +40,22 @@ export default async function IndexPage({ params: { lang } }) {
   return (
     <section className="container flex flex-col items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-center gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Biases {lang}
+        <h1 className="text-3xl font-extrabold leading-tight md:text-4xl">
+          {dictionary.biases}
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          A list of cognitive biases and the translation of it with few
-          examples.
+          {dictionary.description}
         </p>
         <Link className="mt-4" href={lang === "en" ? "ar" : "en"}>
           <Button >
-            {lang === "en" ? "arabic" : "english"}
+            {lang === "en" ? "Arabic" : "English"}
           </Button>
         </Link>
         {/* language switcher by links */}
 
 
       </div>
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">{/* user the cardList */ cardList}</div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">{/* user the cardList */ cardList}</div>
     </section>
   )
 }
